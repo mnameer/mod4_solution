36da9af0d831436c29bd58083c90e4630f4f4e43
mnameer <67372417+mnameer@users.noreply.github.com>
2020-07-07 05:23:52 -0700

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -59,7 +59,7 @@ for (var i = 0; i < names.length; i++) {
   if (firstLetter === 'j') {
     byeSpeaker.speak(names[i]);
   } else {
-    helloSpeaker.speak(names[i]);
+    HelloSpeaker.speak(names[i]);
   }
 }
 
